7a2a1cf9f317451d0d51aa2368aae4e413694beb
Anuja-Aliveli <a.anuja54@gmail.com>
2024-07-14 18:30:05 +0530

    Implemented register password not match error

--- src/components/Authentication/Register.tsx
+++ src/components/Authentication/Register.tsx
@@ -12,7 +12,7 @@ import {
   Typography,
 } from '@mui/material';
 import { registerErrorsInterface } from './authInterface';
-import { FIELD_REQUIRED } from '../../utils/constants';
+import { FIELD_REQUIRED, PASSWORD_NOT_MATCHED } from '../../utils/constants';
 import './auth.css';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
@@ -25,6 +25,8 @@ const Register = () => {
       reEnterPassword: false,
     },
   );
+  const [isPasswordMatchError, setIsPasswordMatchError] =
+    useState<boolean>(false);
 
   const registerContexDetails = useContext(AuthContext);
 
@@ -68,6 +70,10 @@ const Register = () => {
     if (value.trim() !== '') {
       setRegisterErrors((prevErrors) => ({ ...prevErrors, [field]: false }));
     }
+
+    if (field === 'password' || field === 'reEnterPassword') {
+      setIsPasswordMatchError(false);
+    }
   };
 
   const handleSignUpSubmit = (event: any) => {
@@ -84,137 +90,142 @@ const Register = () => {
 
     const hasErrors = Object.values(newErrors).some((error) => error);
 
-    if (!hasErrors) {
+    if (password !== '' && reEnterPassword !== '') {
+      const isPasswordMatched = password === reEnterPassword;
+      setIsPasswordMatchError(!isPasswordMatched);
+    }
+
+    if (!hasErrors && !isPasswordMatchError) {
       onRegister();
     }
   };
 
   return (
-    <>
-      <Container
-        component="main"
-        maxWidth="xs"
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        height: '100% !important',
+      }}>
+      <Box
         sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
           height: '100% !important',
         }}>
+        <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+          Sign up
+        </Typography>
         <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            height: '100% !important',
-          }}>
-          <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            sx={{ mt: 3 }}
-            onSubmit={handleSignUpSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  className="fieldName"
-                  error={registerErrors.userName}
-                  autoComplete="given-name"
-                  name="User Name"
-                  fullWidth
-                  id="userName"
-                  label="User Name"
-                  autoFocus
-                  helperText={registerErrors.userName && FIELD_REQUIRED}
-                  value={userName}
-                  onChange={(e) =>
-                    handleFieldChange('userName', e.target.value)
-                  }
-                  inputProps={{ className: 'fieldName' }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  className="fieldName"
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  error={registerErrors.email}
-                  helperText={registerErrors.email && FIELD_REQUIRED}
-                  value={email}
-                  onChange={(e) => handleFieldChange('email', e.target.value)}
-                  inputProps={{ className: 'fieldName' }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  className="fieldName"
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type={showPassword ? 'text' : 'password'}
-                  id="password"
-                  autoComplete="new-password"
-                  error={registerErrors.password}
-                  helperText={registerErrors.password && FIELD_REQUIRED}
-                  value={password}
-                  onChange={(e) =>
-                    handleFieldChange('password', e.target.value)
-                  }
-                  inputProps={{ className: 'fieldName' }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end">
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  className="fieldName"
-                  fullWidth
-                  name="reEnterpassword"
-                  label="ReEnter Password"
-                  type="password"
-                  id="reEnterPassword"
-                  autoComplete="reEnter-password"
-                  error={registerErrors.reEnterPassword}
-                  helperText={registerErrors.reEnterPassword && FIELD_REQUIRED}
-                  value={reEnterPassword}
-                  onChange={(e) =>
-                    handleFieldChange('reEnterPassword', e.target.value)
-                  }
-                  inputProps={{ className: 'fieldName' }}
-                />
-              </Grid>
+          component="form"
+          noValidate
+          sx={{ mt: 3 }}
+          onSubmit={handleSignUpSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                className="fieldName"
+                error={registerErrors.userName}
+                autoComplete="given-name"
+                name="User Name"
+                fullWidth
+                id="userName"
+                label="User Name"
+                autoFocus
+                helperText={registerErrors.userName && FIELD_REQUIRED}
+                value={userName}
+                onChange={(e) => handleFieldChange('userName', e.target.value)}
+                inputProps={{ className: 'fieldName' }}
+              />
             </Grid>
-            <Button
-              className="authButton"
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}>
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                className="fieldName"
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                error={registerErrors.email}
+                helperText={registerErrors.email && FIELD_REQUIRED}
+                value={email}
+                onChange={(e) => handleFieldChange('email', e.target.value)}
+                inputProps={{ className: 'fieldName' }}
+              />
             </Grid>
-          </Box>
+            <Grid item xs={12}>
+              <TextField
+                className="fieldName"
+                fullWidth
+                name="password"
+                label="Password"
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                autoComplete="new-password"
+                error={registerErrors.password}
+                helperText={registerErrors.password && FIELD_REQUIRED}
+                value={password}
+                onChange={(e) => handleFieldChange('password', e.target.value)}
+                inputProps={{ className: 'fieldName' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                className="fieldName"
+                fullWidth
+                name="reEnterpassword"
+                label="ReEnter Password"
+                type="password"
+                id="reEnterPassword"
+                autoComplete="reEnter-password"
+                error={registerErrors.reEnterPassword || isPasswordMatchError}
+                helperText={
+                  registerErrors.reEnterPassword
+                    ? FIELD_REQUIRED
+                    : isPasswordMatchError
+                    ? PASSWORD_NOT_MATCHED
+                    : ''
+                }
+                value={reEnterPassword}
+                onChange={(e) =>
+                  handleFieldChange('reEnterPassword', e.target.value)
+                }
+                inputProps={{ className: 'fieldName' }}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            className="authButton"
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}>
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
-    </>
+      </Box>
+    </Container>
   );
 };
 
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,2 +1,3 @@
 // Form Constants
 export const FIELD_REQUIRED = 'This Field is Required';
+export const PASSWORD_NOT_MATCHED = 'Password did not match';
